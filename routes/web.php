133d263ca1ac2686916436e3d0c13a4a7975fdce
Pavel Ivanov <p.ivanov.spb@gmail.com>
2024-11-30 14:15:04 +0700

    Links CRUD Test Suite - resolves #2

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\LinkController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -15,6 +16,9 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::resource('links', LinkController::class);
+//    Route::resource('issues', IssueController::class)->except(['edit', 'update', 'destroy']);
 });
 
 require __DIR__.'/auth.php';
